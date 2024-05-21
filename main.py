4b48fb281275a4027bc260b348b478d763ad6b34
SbattellaMattia <tiasba01@gmail.com>
2024-05-21 11:09:11 +0200

    gestione eccezioni

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from bs4 import BeautifulSoup
 
 start_time = time.time()
 
-Keyword = "blockchain"
+KEYWORD = "blockchain"
+
+total = 0
+lock_total = threading.Lock()
 
 done = 0
 lock_done = threading.Lock()
@@ -27,16 +30,17 @@ second_iteration = False
 secondary_links = []
 
 trovati = []
+retry = []
 
 
 def find_keyword(html):
-    if Keyword in html:
+    if KEYWORD in html:
         return True
     else:
         return False
 
 
-def find_links(html):
+def find_links(html,sito):
     soup = BeautifulSoup(html, "html.parser")
     links = soup.find_all("a", href=True)
     links_to_other_pages = []
@@ -44,8 +48,9 @@ def find_links(html):
     for link in links:
         href = link["href"]
         if href.endswith(".html") or href.endswith(".htm") or href.endswith(".pdf"):
+            if not href.startswith("http://") and not href.startswith("https://"):
+                href = "https://" + sito
             links_to_other_pages.append(href)
-
     return links_to_other_pages
 
 
@@ -71,27 +76,37 @@ def initialize():
     return parsed_siti_web
 
 
+def reformat(links):
+    for link in links:
+        str(list(link).insert(4, "s"))
+
+
 def do_request(sito):
+    global total
+    with lock_total:
+        total += 1
     try:
         response = requests.get(sito, timeout=30)
         if response.status_code == 200:
-            if find_keyword(response):
+            if find_keyword(response.text.lower()):
                 # risposte[sito] = response.status_code, True
                 trovati.append(sito)
             elif not second_iteration:
-                for link in find_links(response.text):
-                    secondary_links.append(link)
-            # html_list.append(response.text)
+                for link in find_links(response.text,sito):
+                    if not link in secondary_links:
+                        secondary_links.append(link)
             print(f"HTML ottenuto per {sito}")
             global done
             with lock_done:
                 done += 1
         else:
-            risposte[sito] = response.status_code
-            # print(f"Errore {response.status_code} nel caricare {sito}")
-            global errore
-            with lock_errore:
-                errore += 1
+            if response.status_code == 403:
+                retry.append(sito)
+            else:
+                risposte[sito] = response.status_code
+                global errore
+                with lock_errore:
+                    errore += 1
     except requests.exceptions.Timeout:
         # print("Timed out")
         risposte[sito] = "TIMEOUT"
@@ -99,12 +114,17 @@ def do_request(sito):
         with lock_time_out:
             time_out += 1
     except Exception as e:
-        risposte[sito] = str(e)
-        # print(f"Eccezione durante il recupero di {sito}: {str(e)}")
-        global eccezione
-        with lock_eccezione:
-            eccezione += 1
-
+        if "HTTPS" in e:
+            retry.append(sito)
+        else:
+            risposte[sito] = str(e)
+            global eccezione
+            with lock_eccezione:
+                eccezione += 1
+        # risposte[sito] = str(e)
+        # global eccezione
+        # with lock_eccezione:
+        #     eccezione += 1
 
 def parallel_request(method, list):
     # Initialize ThreadPoolExecutor and use it to call parse_page() in parallel
@@ -113,15 +133,19 @@ def parallel_request(method, list):
 
 
 siti_web = initialize()
-parallel_request(do_request, siti_web)
-siti_web = secondary_links.copy()
+parallel_request(do_request, siti_web[:10])
+
+parallel_request(do_request, retry)
+
 second_iteration = True
-parallel_request(do_request, siti_web)
+parallel_request(do_request, secondary_links)
+
+
 
 
 # Tempistiche
 print("--- %s seconds ---" % (time.time() - start_time))
-print(f"\nRecuperati: {done}\nErrori:{errore}\nTimeout: {time_out}\nEccezioni:{eccezione}")
+print(f"Totali:\n\t-Iterazioni: {total}\n\t-Siti web: {len(siti_web)}\n\t-Link: {len(secondary_links)}\n\t-Retry: {len(retry)} \nRecuperati: {done}\nErrori:{errore}\nTimeout: {time_out}\nEccezioni non considerate:{eccezione}")
 
 # Creazione di un DataFrame da un dizionario
 df_finale = pd.DataFrame(list(risposte.items()), columns=["Url", "Status"])
