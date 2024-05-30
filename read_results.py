bb544482be8a71f5693087fdfdc32ce2ea467e27
MrPio <valeriomorelli50@gmail.com>
2024-05-31 01:18:41 +0200

    final.csv and final2.csv

--- read_results.py
+++ read_results.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv('output.csv')
+imprese = pd.read_excel('Imprese.xlsx')
+imprese2 = pd.read_csv('scraper/output.csv')
+# df = pd.read_csv('output.csv')
+df2 = pd.read_csv('output2.csv', low_memory=False)
 
-df[(df['status'] == 'Status.SUCCESS') & (df['has_keyword'])][['base_url']].drop_duplicates().to_csv('result.csv',index=False)
+# df = df[(df['status'] == 'Status.SUCCESS') & (df['has_keyword'])] \
+#     .drop_duplicates(subset='base_url', keep='first')
+df2 = df2[(df2['status'] == 'Status.SUCCESS') & (df2['has_keyword'])]\
+    .drop_duplicates(subset='base_url', keep='first')
+
+# imprese = pd.merge(imprese, df, left_on='Website', right_on='base_url', how='left')
+# imprese.to_csv('final.csv', index=False)
+
+imprese2 = pd.merge(imprese2, df2, left_on='url', right_on='base_url', how='left')
+imprese2.to_csv('final2.csv', index=False)
